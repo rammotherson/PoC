332944284a92270a876942a4cb6da1dda7d8840b
ram <ram.motherson@gmail.com>
2020-03-26 11:51:55 +0000

    addded new test case

--- src/webparts/helloWorld/tests/helloWorld.spec.ts
+++ src/webparts/helloWorld/tests/helloWorld.spec.ts
@@ -1,6 +1,14 @@
 import 'jest';
 
-describe("Demo_TestCase", () => {  test(" add ywo numbers ", () => { const result = 1 + 3;
-    expect(result).toBe(4); // fluent API
+describe("Demo_TestCase1", () => {  test(" add two numbers ", () => { const result = 1 + 3;
+    expect(result).toBe(4); 
   });
+}
+
+);
+describe("Demo_TestCase2", () => {  test(" Sub two numbers ", () => { const result = 4 - 3;
+  expect(result).toBe(1); 
 });
+}
+
+);
