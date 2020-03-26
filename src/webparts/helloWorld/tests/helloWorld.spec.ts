7089ba51cfa30bdc8e69ce37b9e8a03a3fc2ae77
ram <ram.motherson@gmail.com>
2020-03-26 09:56:51 +0000

    Done

--- src/webparts/helloWorld/tests/helloWorld.spec.ts
+++ src/webparts/helloWorld/tests/helloWorld.spec.ts
@@ -1,6 +1,6 @@
 import 'jest';
 
 describe("webPartName", () => {  test("should add numbers Sync fluent", () => { const result = 1 + 3;
-    expect(result).toBe(4); // fluent API
+    expect(result).toBe(5); // fluent API
   });
 });
